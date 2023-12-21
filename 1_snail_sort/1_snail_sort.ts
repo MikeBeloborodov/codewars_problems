0d172f6e64a021aff6ddfd97d1870c0e7278e4c2
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2023-12-22 02:58:01 +0400

    finished snail sort

---
+++ 1_snail_sort/1_snail_sort.ts
@@ -0,0 +1,43 @@
+/*
+Snail Sort
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+For better understanding, please follow the numbers of the next array consecutively:
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+  * */
+
+const snailSort = (array: any): number[] => {
+  const result: number[] = [];
+
+  while (array.length) {
+    result.push(...array.shift());
+    array.forEach((item: any) => result.push(item.pop()));
+    array.reverse().forEach((item: any) => item.reverse());
+  }
+
+  return result;
+};
+
+const case1 = snailSort([]);
+console.assert(JSON.stringify(case1) === JSON.stringify([]), "case1");
+
+const case2 = snailSort([[1]]);
+console.assert(JSON.stringify(case2) === JSON.stringify([1]), "case2");
+
+const case3 = snailSort([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+]);
+console.assert(
+  JSON.stringify(case3) === JSON.stringify([1, 2, 3, 6, 9, 8, 7, 4, 5]),
+  "case3",
+);

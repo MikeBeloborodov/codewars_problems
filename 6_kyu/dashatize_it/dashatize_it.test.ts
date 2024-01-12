716dba19d4438f2efb203fc8ba2da9d93160c871
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-01-12 17:31:57 +0400

    refactor dashatize

---
+++ 6_kyu/dashatize_it/dashatize_it.test.ts
@@ -0,0 +1,13 @@
+import { dashatize } from "./dashatize_it";
+
+test("test case 1", () => {
+  expect(dashatize(274)).toBe("2-7-4");
+});
+
+test("test case 2", () => {
+  expect(dashatize(974302)).toBe("9-7-4-3-02");
+});
+
+test("test case 3", () => {
+  expect(dashatize(5311)).toBe("5-3-1-1");
+});
--- 6_kyu/dashatize_it/dashatize_it.ts
+++ 6_kyu/dashatize_it/dashatize_it.ts
@@ -13,7 +13,7 @@ dashatize(6815) -> '68-1-5'
 
 // my solution
 
-const dashatize = (num: number): string | "NaN" => {
+export const dashatize = (num: number): string | "NaN" => {
   let res = Math.abs(num)
     .toString()
     .split("")
@@ -25,19 +25,10 @@ const dashatize = (num: number): string | "NaN" => {
   return Number.isNaN(num) ? "NaN" : res;
 };
 
-const case1 = dashatize(274);
-console.assert(case1 === "2-7-4", "case1");
-
-const case2 = dashatize(974302);
-console.assert(case2 === "9-7-4-3-02", "case2");
-
-const case3 = dashatize(5311);
-console.assert(case3 === "5-3-1-1", "case3");
-
 // regexp solution
-function dashatize2(num: number) {
-  return String(num)
-    .replace(/([13579])/g, "-$1-")
-    .replace(/--+/g, "-")
-    .replace(/(^-|-$)/g, "");
-}
+//function dashatize2(num: number) {
+//  return String(num)
+//    .replace(/([13579])/g, "-$1-")
+//    .replace(/--+/g, "-")
+//    .replace(/(^-|-$)/g, "");
+//}

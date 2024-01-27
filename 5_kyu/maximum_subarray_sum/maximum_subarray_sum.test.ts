657df3c85e77ea928fcafcbb350da940c5890349
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-01-27 17:03:20 +0400

    some minor fixes of styles and tests

--- 5_kyu/maximum_subarray_sum/maximum_subarray_sum.test.ts
+++ 5_kyu/maximum_subarray_sum/maximum_subarray_sum.test.ts
@@ -7,4 +7,5 @@ describe("test cases", () => {
         7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43,
       ]),
     ).toBe(155));
+  it("should return 0", () => expect(maxSequence([])).toBe(0));
 });
--- 8_kyu/odd_function/README.md
+++ 8_kyu/odd_function/README.md
@@ -3,3 +3,4 @@
 https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/
 
 ![description](./description.jpg "Description")
+
